25cc2b1b1df1c02507c47b65e5730c756dd2a02f
Vidhyanshu Kumar <vidhyanshukumar@Vidhyanshus-MacBook-Air.local>
2026-02-11 23:18:52 +0530

    Initial
    :wq

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/src/components/common/Footer";
+import {Skiper19} from "@/src/components/ui/stroke"
+export default function Home() {
+  return (
+    <>
+    
+      <Skiper19 />
+      
+    </>
+  );
+}
---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import ScrollVelocity from "@/src/components/ui/bottom"; 
+// (your scroll velocity component is named Footer right now)
+
+export default function FooterWrapper() {
+  return (
+          <footer className="w-full bg-black text-white mt-20">
+      <ScrollVelocity
+        texts={[
+          "IEEE COMPUTER SOCIETY",
+          "INNOVATION",
+          "TECHNOLOGY",
+          "FUTURE"
+        ]}
+      />
+    </footer>
+  );
+}
